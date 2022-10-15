e623c4bbab8ecf7635df06a79bfeb903e20f050f
Wooryeong <wooryeongg@gmail.com>
2022-10-15 18:42:30 +0900

    1부터 50의 자연수 중 짝수를 구하는 함수

---
+++ test.js
@@ -0,0 +1,12 @@
+function evenNumber() {
+    let even = [];
+    for (let i = 1; i <= 50; i++) {
+        if (i % 2 === 0) {
+            even.push(i);
+        }
+    }
+    return even;
+}
+
+evenNumber();
+console.log(evenNumber());
